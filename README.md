f0c2aade8f286bf469786d709965ac57cb7b6b6c
Andrea Fossati <anfossat@biol-sysbc-hpx-docking-1-021.ethz.ch>
2020-03-06 14:52:09 +0100

    [CODE] add mist.py

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The implementation in wd_comppass.py uses as input a 2D adjacency matrix in the 
 | Prey1          | Prey1Bait1     | Prey1Bait2     |
 | Prey2          | Prey1Bait1     | Prey2Bait2     |
 | Prey3          | Prey3Bait1     | Prey3Bait2     |
-| Prey4          | Prey4Bait2     | Prey4Bait2     |
+| Prey4          | Prey4Bait1     | Prey4Bait2     |
 
 
 This table needs to be a 2D numpy array. Following calculation of WD score for the provided matrix, a simulated dataset is generated to be able to assess false discovery rate.
@@ -42,3 +42,39 @@ distr[wd_scores>0]=0
 ## MiST
 
 Developed by the [Krogan lab](https://kroganlab.ucsf.edu) this metric takes into account variability, weighted sum across experiments and uniqueness of a particular bait-prey interaction. mist.py is ported from [this implementation](https://github.com/kroganlab/mist)
+
+The implementation in mist.py uses as input a 2D adjacency matrix in the form
+
+|                | Prey1          | Prey2          |
+| :------------- | :------------- | :------------- |
+| Bait1          | Prey1Bait1     | Prey2Bait2     |
+| Bait1          | Prey1Bait1     | Prey2Bait2     |
+| Bait2          | Prey2Bait2     | Prey2Bait2     |
+| Bait2          | Prey2Bait2     | Prey2Bait2     |
+
+
+The input format is a Panda dataframe.
+In this example two replicate pulldown with two baits (bait1 and bait2) are performed. A complete example is available in test/mist_test.txt
+
+Two mode can be used for calculation of mist scores
+
+*mode* combination of abundance, reproducibility and specificity for calculating mist score. can be either *_fixed_* or *_PCA_*.
+In fixed the product of abundance, reproducibility and specificity is calculated. In PCA mode a PCA is performed and the first component is used as mist score.
+Finally, K mean with 2 components is used to separate false and true interaction and another column indicating the cluster is returned
+
+To run tests and see input format:
+
+```
+import numpy as np
+import mist as mist
+
+distr = mist.create_test_data()
+mist = mist.calc_mist_matrix(distr, weights='PCA', plot=True)
+
+```
+
+alternatively test can be run from a terminal
+
+```
+python3 mist.py
+```
--- pyAPMS/mist.py
+++ pyAPMS/mist.py
@@ -7,45 +7,103 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
 
-def pca_extract(X, plot=True):
+
+def plot_pca(X_svd):
+    fig = plt.figure(1, figsize=(4, 3))
+    plt.clf()
+    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+
+    plt.cla()
+    y_pred = KMeans(n_clusters=2, random_state=0).fit_predict(X_svd)
+
+    # Reorder the labels to have colors matching the cluster results
+    ax.scatter(X_svd[:, 0], X_svd[:, 1], X_svd[:, 2], c=y_pred, edgecolor='k')
+
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
+    plt.show()
+
+
+def calc_abu_rep(q_df):
     """
-    calculate composite score by take pca first dimension
-     Args:
-        Nx3 array where colums are abundance, reproducibility, and specificity
-     Returns:
-        N x1 array where the composite score is calculated
+    calculate abundance and reproducibility
+    abundance is defined as mean of bait-prey over all replicates bound [0,1]
+    reproducibility is the normalized entropy of the vector in repl bound [0,1]
+    Args:
+        - q_df:  grouped dataframe for bait across multiple replicates
+    Returns:
+        - abu: np.array of abundance for that bait
+        - rep: np.array of rep for bait
+    Raises:
     """
-    sc = StandardScaler()
-    X_sc = sc.fit_transform(X)
-    cov_mat = np.cov(X_sc.T)
-    eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-    tot = sum(eigen_vals)
-    var_exp = [(i/tot) for i in sorted(eigen_vals, reverse=True)]
-    cum_var_exp = np.cumsum(var_exp)
-    if plot:
-        plt.bar(range(1, X.shape[0]+1), var_exp, alpha=0.5, align='center', label='individual explained variance')
-        plt.step(range(1, X.shape[0]+1), cum_var_exp, where='mid', label='cumulative explained variance')
-        plt.ylabel('Explained variance ratio')
-        plt.xlabel('Principal component index')
-        plt.legend(loc='best')
-        plt.tight_layout()
-        plt.show()
-    eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) for i in range(len(eigen_vals))]
-    eigen_pairs.sort(key=lambda k:k[0], reverse=True)
-    # now we got the transformation matrix
-    w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
-                eigen_pairs[1][1][:, np.newaxis]))
-    return X_sc.dot(w)
+    # works on i.e on all preys in one go
+    # is grouped by bait
+    q = q_df.values
+    abu = np.mean(q, axis=0).flatten()
+    #abu needs to have same number of rows but 1 col
+    norm_entr = lambda x: np.sum(x*np.log(x))/np.log2(x.shape[0])**-1
+    #entr needs to have same number of rows but 1 col
+    rep = np.apply_along_axis(norm_entr, 0, q).flatten()
+    metr_df = pd.DataFrame(np.array([abu, rep]), columns=list(q_df))
+    metr_df = metr_df.fillna(0, inplace=False).T
+    metr_df.columns = ['abu', 'repl']
+    return metr_df
 
 
+def create_test_data():
+    """
+    create input format
+    """
+    rnd = np.random.uniform(0, 100, size=(9,100))
+    rnd = pd.DataFrame(rnd)
+    rnd.index = [1,1,1,2,2,2,3,3,3]
+    rnd.columns = range(0,100)
+    rnd['Bait'] = rnd.index
+    return rnd
 
 
-def test_():
-    distr = np.random.rand(100,100)
-    mist = pca_extract(distr)
-    print(mist)
+def calc_mist_matrix(df, weights='PCA'):
+    """
+    get a NxM matrix and calculate wd then for iteration creates dummy matrix
+    and score them to get distribution of simulated interactors for each bait
+    Args:
+        m stats matrix following http://besra.hms.harvard.edu/ipmsmsdbs/cgi-bin/tutorial.cgi format
+        iteration number of iteration for generating simulated distribution
+        quantile to filter interactors for
+        norm quantile based normalization of interaction
+        plot boolean for plotting distribution of real and simulated data
+    Returns:
+        wd
+    """
+    df.set_index('Bait', inplace=True)
+    # convert to quantities
+    q = df.apply(lambda x: x/np.sum(x), axis=1)
+    metr = df.groupby(['Bait']).apply(calc_abu_rep).reset_index()
+    specificity = lambda x: x/np.sum(x)
+    metr['sp'] = metr.groupby(['level_1'])['abu'].apply(specificity)
+    X = metr[['abu','sp', 'repl']].values
+    if weights == 'fixed':
+        metr['cl'] = KMeans(n_clusters=2, random_state=0).fit_predict(X)
+        metr['mist_score'] = metr['abu']*metr['repl']*metr['sp']
+    elif weights == 'PCA':
+        X = scale(X, axis=0, with_mean=True, with_std=True)
+        pca = PCA()
+        pca_out = pca.fit_transform(X)
+        plot_pca(pca_out)
+        metr['cl'] = KMeans(n_clusters=2, random_state=0).fit_predict(pca_out)
+        metr['mist_score'] = pca_out[:, 0]
+    return metr
+
+
+def run_test():
+    calc_mist_matrix(create_test_data())
 
 if __name__ == '__main__':
-    test_()
+    run_test()
